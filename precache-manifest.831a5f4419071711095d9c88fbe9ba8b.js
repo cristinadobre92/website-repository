99d517e30febcb8a7c129168010fbb5615efc421
Cristina Dobre <crisfdobre@gmail.com>
2020-04-08 16:30:48 +0200

    Updates

---
+++ precache-manifest.831a5f4419071711095d9c88fbe9ba8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73897f2d7b1e68f5fa601bc63d42b56e",
+    "url": "/website-repository/index.html"
+  },
+  {
+    "revision": "bf76935054de0f422474",
+    "url": "/website-repository/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6cf0f7c26271f6236167",
+    "url": "/website-repository/static/js/2.1e50411a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-repository/static/js/2.1e50411a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf76935054de0f422474",
+    "url": "/website-repository/static/js/main.be0991fa.chunk.js"
+  },
+  {
+    "revision": "241664333ed62ad13fca",
+    "url": "/website-repository/static/js/runtime-main.0b2fff3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-repository/static/media/logo.5d5d9eef.svg"
+  }
+]);
