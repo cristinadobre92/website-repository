5d5dd2516ebf804f577307005279b71fe01b217f
Cristina Dobre <crisfdobre@gmail.com>
2020-04-14 21:34:38 +0200

    add docs/ folder

---
+++ docs/precache-manifest.48bd57c460629cb6a17d7c10cd922318.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba35c3b99278a229bb7c9ce6276b18e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d424ae24386fc855df5",
+    "url": "/static/css/2.20155695.chunk.css"
+  },
+  {
+    "revision": "d9c0c5282c932daa2f70",
+    "url": "/static/css/main.dc6d59e5.chunk.css"
+  },
+  {
+    "revision": "1d424ae24386fc855df5",
+    "url": "/static/js/2.df72c59e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.df72c59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c0c5282c932daa2f70",
+    "url": "/static/js/main.d56e9ad4.chunk.js"
+  },
+  {
+    "revision": "cc0fd0f39b88af6399c1",
+    "url": "/static/js/runtime-main.96d9dfaa.js"
+  },
+  {
+    "revision": "a58794908e84f5004250ab11c344ad7b",
+    "url": "/static/media/aintmuch.a5879490.jpg"
+  }
+]);
