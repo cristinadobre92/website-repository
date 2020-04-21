a5d0a7f6d02807f4fe6b438103ed73468520cd54
Cristina Dobre <crisfdobre@gmail.com>
2020-04-21 12:41:45 +0200

    Fixed homepage and filled professional page

---
+++ docs/build/precache-manifest.5a88f494e8351ef88fa429ec4291660c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfba36a332d11cc1c94836cf08ad6f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3dcc6341d2de6ab31b",
+    "url": "/static/css/2.20155695.chunk.css"
+  },
+  {
+    "revision": "f5010296b54c0c307907",
+    "url": "/static/css/main.3ce0bfd0.chunk.css"
+  },
+  {
+    "revision": "ee3dcc6341d2de6ab31b",
+    "url": "/static/js/2.cce4a715.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.cce4a715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5010296b54c0c307907",
+    "url": "/static/js/main.49795d57.chunk.js"
+  },
+  {
+    "revision": "cc0fd0f39b88af6399c1",
+    "url": "/static/js/runtime-main.96d9dfaa.js"
+  },
+  {
+    "revision": "a58794908e84f5004250ab11c344ad7b",
+    "url": "/static/media/aintmuch.a5879490.jpg"
+  }
+]);
