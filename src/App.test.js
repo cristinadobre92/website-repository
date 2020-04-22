e0d1c880e2b353b3da3308e32242876d6b0f13a8
Michal Šimon <53786124+michal-simon@users.noreply.github.com>
2020-04-22 15:50:19 +0200

    @ missing import

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { except } from '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
 test('renders learn react link', () => {
